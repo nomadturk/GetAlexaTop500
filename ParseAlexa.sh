be5b26a2bfcd0f1601c6fbdde01c30a009374ed0
M. Omer Golgeli <az@cokh.net>
2015-02-04 03:58:13 +0200

    Update ParseAlexa.sh

--- ParseAlexa.sh
+++ ParseAlexa.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Let's get the Global Top 2500 sites from alexa with the command:
 curl -s -O http://s3.amazonaws.com/alexa-static/top-1m.csv.zip ; unzip -q -o top-1m.csv.zip top-1m.csv ; head -2500 top-1m.csv | cut -d, -f2 | cut -d/ -f1 > AlexaTop2500.txt
 
